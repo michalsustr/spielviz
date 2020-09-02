616e50e3b99d34eafa5c8fd0ba4f59f98494667c
Michal Sustr <michal.sustr@aic.fel.cvut.cz>
2020-09-02 05:37:25 +0200

    Abstract away tagged view from history view.

--- spielviz/ui/history_view.py
+++ spielviz/ui/history_view.py
@@ -1,4 +1,5 @@
 import spielviz.config as cfg
+from spielviz.ui.tagged_view import TaggedView
 import pyspiel
 from gi.repository import Gtk, Pango
 
@@ -18,34 +19,14 @@ def player_to_str(player: int):
     raise AttributeError(f"Player not found: {player}")
 
 
-class HistoryView:
+class HistoryView(TaggedView):
   """
   Render information about the current pyspiel.State in a non-game-specific
   (general) way, like the current history, player(s) to move etc.
   """
 
   def __init__(self, container: Gtk.TextView):
-    self.container = container
-    self.textbuffer = Gtk.TextBuffer()
-    self.textbuffer.set_text("")
-    self.container.set_buffer(self.textbuffer)
-
-    self._tag_note = self.textbuffer.create_tag(
-        "n", foreground="#999999")
-    self._tag_section = self.textbuffer.create_tag(
-        "b", weight=Pango.Weight.BOLD)
-    self._tag_player = [
-      self.textbuffer.create_tag(f"p{p}", foreground=color)
-      for p, color in enumerate(cfg.PLAYER_COLORS)
-    ]
-    self._tag_invalid = self.textbuffer.create_tag(
-        "inv", foreground=cfg.INVALID_PLAYER_COLOR)
-    self._tag_terminal = self.textbuffer.create_tag(
-        "ter", foreground=cfg.TERMINAL_COLOR)
-    self._tag_chance = self.textbuffer.create_tag(
-        "chn", foreground=cfg.CHANCE_COLOR)
-    self._tag_simultaneous = self.textbuffer.create_tag(
-        "sim", foreground=cfg.SIMULTANEOUS_PLAYER_COLOR)
+    super(HistoryView, self).__init__(container)
 
   def update(self, state: pyspiel.State):
     self._clear()
@@ -103,36 +84,3 @@ class HistoryView:
         aoh = pyspiel.ActionObservationHistory(player, state)
         for act_or_obs in aoh.history():
           self._appendln(str(act_or_obs))
-
-  def _append(self, text: str, *tags):
-    if tags:
-      self.textbuffer.insert_with_tags(
-          self.textbuffer.get_end_iter(), text, *tags)
-    else:
-      self.textbuffer.insert(self.textbuffer.get_end_iter(), text)
-
-  def _appendln(self, text: str, *tags):
-    self._append(text + "\n", *tags)
-
-  def _appendln_pl(self, text: str, player: int):
-    self._appendln(text, self._player_tag(player))
-
-  def _append_pl(self, text: str, player: int):
-    self._append(text, self._player_tag(player))
-
-  def _player_tag(self, player: int):
-    if player >= 0:
-      return self._tag_player[player]
-    elif player == pyspiel.PlayerId.INVALID:
-      return self._tag_invalid
-    elif player == pyspiel.PlayerId.TERMINAL:
-      return self._tag_terminal
-    elif player == pyspiel.PlayerId.CHANCE:
-      return self._tag_chance
-    elif player == pyspiel.PlayerId.SIMULTANEOUS:
-      return self._tag_simultaneous
-    else:
-      raise AttributeError(f"Player not found: {player}")
-
-  def _clear(self):
-    self.textbuffer.set_text("")
